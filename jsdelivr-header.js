27935e0a789d1ed349ae11967e2459b693b1b8ea
Karmiktyagi <karmiktyagi@gmail.com>
2026-07-22 16:18:43 +0530

    jsdelivr-header.js

---
+++ jsdelivr-header.js
@@ -0,0 +1,6 @@
+/**
+ * Minified by jsDelivr using Terser v5.39.0.
+ * Original file: /npm/canvas-confetti@1.9.3/dist/confetti.browser.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
